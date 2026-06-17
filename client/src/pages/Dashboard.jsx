f1fd1b78095900a456b5a6c57ba445e84137399b
Sanchit Gupta <sanchit221206@gmail.com>
2026-06-17 04:56:50 +0000

    feat(resume-preview): implement resume preview for viewing generated resumes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -67,25 +67,26 @@ function Dashboard() {
 
 
     <hr className="border-slate-300 my-6 sm:w-[305px]"/>
-   <div className="grid grid-cols-2 sm:flex flex-wrap gap-4 ">
-  {allResumes.map((resume, index)=>{
-    const baseColor = colors[index % colors.length];
-    return (
-      <button key={index} onClick={()=>navigate(`/app/builder/${resume.id}`)}className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer' style={{background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)`, borderColor: baseColor + '40'}}>
 
-        <FilePenLineIcon className="size-7 group-hover:scale-105 transition-all " style={{ color: baseColor }}/>
-        <p className='text-sm group-hover:scale-105 transition-all  px-2 text-center' style={{ color: baseColor }}>{resume.title}</p>
-        <p className='absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 text-center' style={{ color: baseColor + '90' }}>
-          Updated on {new Date(resume.updatedAt).toLocaleDateString()}
-        </p>
-        <div  onClick={e=>e.stopPropagation()} className='absolute top-1 right-1  flex items-center  '>
-  <TrashIcon onClick={()=>deleteResume(resume._id)}className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors"/>
-  <PencilIcon onClick={()=>{setEditResumeId(resume._id);setTitle(resume.title)}} className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors"/>
-</div>
-      </button>
-    )
-  })}
-</div>
+    <div className="grid grid-cols-2 sm:flex flex-wrap gap-4 ">
+        {allResumes.map((resume, index)=>{
+          const baseColor = colors[index % colors.length];
+          return (
+            <button key={index} onClick={()=> navigate(`/app/builder/${resume._id}`)} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer' style={{background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)`, borderColor: baseColor + '40'}}>
+
+              <FilePenLineIcon className="size-7 group-hover:scale-105 transition-all " style={{ color: baseColor }}/>
+              <p className='text-sm group-hover:scale-105 transition-all  px-2 text-center' style={{ color: baseColor }}>{resume.title}</p>
+              <p className='absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 text-center' style={{ color: baseColor + '90' }}>
+                 Updated on {new Date(resume.updatedAt).toLocaleDateString()}
+              </p>
+              <div onClick={e=> e.stopPropagation()} className='absolute top-1 right-1 group-hover:flex items-center hidden'>
+                <TrashIcon onClick={()=>deleteResume(resume._id)} className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors"/>
+                <PencilIcon onClick={()=> {setEditResumeId(resume._id); setTitle(resume.title)}} className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors"/>
+              </div>
+            </button>
+          )
+        })}
+      </div>
       
       {showCreateResume && (
   <form onSubmit={createResume} onClick={() => setShowCreateResume(false)} 
