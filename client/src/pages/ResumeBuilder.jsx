13855cae88f3ec8fb562a7017807d26c2f2d6c75
Sanchit Gupta <sanchit221206@gmail.com>
2026-06-17 04:08:11 +0000

    feat(resume-builder): add save changes button

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -134,6 +134,11 @@ setRemoveBackground={setRemoveBackground} />
      
 
 </div>
+
+
+<button  className='bg-gradient-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+                Save Changes
+              </button>
     
     
     
