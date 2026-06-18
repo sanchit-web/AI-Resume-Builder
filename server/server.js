0b723a1c49b00ed89622c92b339b78aaa6c0f6ff
Sanchit Gupta <sanchit221206@gmail.com>
2026-06-18 07:38:09 +0000

    feat(db): add MongoDB connection setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Database connection
+await connectDB()
 
 app.use(express.json());
 app.use(cors());
