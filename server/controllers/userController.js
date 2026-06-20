df7811fa4060cfd37b44b07d9b8862dc55201fc6
Sanchit Gupta <sanchit221206@gmail.com>
2026-06-20 02:46:22 +0000

    feat: add protected route to fetch user resumes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import Resume from "../models/Resume.js";
 
 
 
@@ -91,4 +92,20 @@ export const getUserById = async (req, res) => {
         return res.status(400).json({message: error.message})
     }
 }
+
+// controller for getting user resumes
+// GET: /api/users/resumes
+export const getUserResumes = async (req, res) => {
+    try {
+        const userId = req.userId;
+
+        // return user resumes
+        const resumes = await Resume.find({userId})
+        return res.status(200).json({resumes})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
+
 
