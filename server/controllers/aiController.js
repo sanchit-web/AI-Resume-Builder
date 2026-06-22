dc11a06d4058035f9f28d5a57c4ed4f1229edd57
Sanchit Gupta <sanchit221206@gmail.com>
2026-06-22 03:52:58 +0000

    feat: add AI-powered resume summary and job description enhancement

---
+++ server/controllers/aiController.js
@@ -0,0 +1,60 @@
+import Resume from "../models/Resume.js";
+import ai from "../configs/ai.js";
+
+// controller for enhancing a resume's professional summary
+// POST: /api/ai/enhance-pro-sum
+export const enhanceProfessionalSummary = async (req, res) => {
+    try {
+        const { userContent } = req.body;
+
+        if(!userContent){
+            return res.status(400).json({message: 'Missing required fields'})
+        }
+
+       const response = await ai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+            messages: [
+                { role: "system", content: "You are an expert in resume writing. Your task is to enhance the professional summary of a resume. The summary should be 1-2 sentences also highlighting key skills, experience, and career objectives. Make it compelling and ATS-friendly. and only return text no options or anything else." },
+                {
+                    role: "user",
+                    content: userContent,
+                },
+    ],
+        })
+
+        const enhancedContent = response.choices[0].message.content;
+        return res.status(200).json({enhancedContent})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
+// controller for enhancing a resume's job description
+// POST: /api/ai/enhance-job-desc
+export const enhanceJobDescription = async (req, res) => {
+    try {
+        const { userContent } = req.body;
+
+        if(!userContent){
+            return res.status(400).json({message: 'Missing required fields'})
+        }
+
+       const response = await ai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+            messages: [
+                { role: "system",
+                 content: "You are an expert in resume writing. Your task is to enhance the job description of a resume. The job description should be only in 1-2 sentence also highlighting key responsibilities and achievements. Use action verbs and quantifiable results where possible. Make it ATS-friendly. and only return text no options or anything else." },
+                {
+                    role: "user",
+                    content: userContent,
+                },
+    ],
+        })
+
+        const enhancedContent = response.choices[0].message.content;
+        return res.status(200).json({enhancedContent})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
