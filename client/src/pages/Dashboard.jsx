367f73e26ccb765119dfb506c808981cd356991f
Sanchit Gupta <sanchit221206@gmail.com>
2026-06-25 08:23:53 +0000

    fix : upload resume button

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -187,8 +187,8 @@ function Dashboard() {
 onChange={(e)=> setResume(e.target.files[0])}/>
 </div>
 
-      <button className='w-full py-2 bg-green-600 text-white rounded 
-      hover:bg-green-700 transition-colors'>
+      <button disabled={isLoading} className='w-full py-2 bg-green-600 text-white rounded 
+      hover:bg-green-700 transition-colors flex items-center justify-center gap-2'>
       {isLoading && <LoaderCircleIcon className='animate-spin size-4 text-white'/>}
       {isLoading ? 'Uploading...' : 'Upload Resume'}
       </button>
